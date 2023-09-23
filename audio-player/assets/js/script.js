8716d57942828079e1bb7fb02122e124209ee168
slawkir <slawkir1984@gmail.com>
2023-09-23 18:38:50 +0400

    feat: add play-pause-function, time-range-function (Sat, Sep 23, 2023 05:38:00 PM)

--- audio-player/assets/js/script.js
+++ audio-player/assets/js/script.js
@@ -1,2 +1,31 @@
-let track = document.querySelector('.audio');
-console.log(track.duration);
+// const
+let audio = document.querySelector('.audio');
+const btnPlayPause = document.querySelector('.play-pause');
+const timeRange = document.getElementById('timerange');
+
+
+// functions
+function togglePlayPause() { 
+  if (audio.paused) {
+    btnPlayPause.className = 'play-pause pause';
+    audio.play();
+  } else { 
+    btnPlayPause.className = 'play-pause play';
+    audio.pause();
+  }
+}
+
+
+
+
+// events
+btnPlayPause.addEventListener('click', togglePlayPause);
+audio.addEventListener('timeupdate', function () {
+  timeRange.value = audio.currentTime / audio.duration;
+  console.log(audio.currentTime);
+  console.log(audio.duration);
+});
+
+// timeRange.addEventListener('click', () => { 
+//   console.log(timeRange.value);
+// });
