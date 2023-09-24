149787078868209680aaef22988f626ab248d0e7
slawkir <slawkir1984@gmail.com>
2023-09-24 15:41:16 +0400

    feat: add feature player (Sun, Sep 24, 2023 02:40:00 PM)

--- audio-player/assets/js/script.js
+++ audio-player/assets/js/script.js
@@ -1,25 +1,79 @@
 // const
 let audio = document.querySelector('.audio');
 
-//менять audio при нажатии на переключения треков
-//создать массив объектов. В объекте: адреса к фалам с музыкой, картинкой, название альбома
 let arrSongs = [
   {
     audioFile: './assets/audio/Linkin_Park.mp3',
-    container: '../img/linkin.jpg',
-    player: '../img/linkin.jpg',
+    container: "url('./assets/img/linkin.jpg')",
+    player: "url('./assets/img/linkin.jpg')",
     singer: 'Linkin Park',
     album: 'Living Things',
-    track: 'Castle Of Glass'
+    composition: 'Castle Of Glass'
   },
   {
     audioFile: './assets/audio/Imany.mp3',
-    container: '../img/imany.png',
-    player: '../img/imany.png',
+    container: "url('./assets/img/imany.png')",
+    player: "url('./assets/img/imany.png')",
     singer: 'Imany',
     album: 'The Shape of a Broken Heart',
-    track: 'You Will Never Know'
-  }
+    composition: 'You Will Never Know'
+  },
+  {
+    audioFile: './assets/audio/Robert.mp3',
+    container: "url('./assets/img/robert.jpg')",
+    player: "url('./assets/img/robert.jpg')",
+    singer: 'Robert Cristian, Alis Shuka',
+    album: 'The Day Before',
+    composition: 'The Day Before'
+  },
+  {
+    audioFile: './assets/audio/Tomas.mp3',
+    container: "url('./assets/img/tomas.jpg')",
+    player: "url('./assets/img/tomas.jpg')",
+    singer: 'Tomas Nevergreen',
+    album: 'Since You Been Gone',
+    composition: 'Since You Been Gone'
+  },
+  {
+    audioFile: './assets/audio/Limp.mp3',
+    container: "url('./assets/img/limp.jpg')",
+    player: "url('./assets/img/limp.jpg')",
+    singer: 'Limp Bizkit',
+    album: 'Greatest Hitz',
+    composition: 'Behind Blue Eyes'
+  },
+  {
+    audioFile: './assets/audio/Purple.mp3',
+    container: "url('./assets/img/purple.jpg')",
+    player: "url('./assets/img/purple.jpg')",
+    singer: 'Purple Disco Machine',
+    album: 'Substitution',
+    composition: 'Substitution'
+  },  
+  {
+    audioFile: './assets/audio/Reamonn.mp3',
+    container: "url('./assets/img/reamonn.jpg')",
+    player: "url('./assets/img/reamonn.jpg')",
+    singer: 'Reamonn',
+    album: 'Tonight',
+    composition: 'Tonight'
+  },  
+  {
+    audioFile: './assets/audio/Scorpions.mp3',
+    container: "url('./assets/img/scorpions.jpg')",
+    player: "url('./assets/img/scorpions.jpg')",
+    singer: 'Scorpions',
+    album: 'Unbreakable',
+    composition: 'Maybe I Maybe You'
+  },  
+  {
+    audioFile: './assets/audio/Lemar.mp3',
+    container: "url('./assets/img/lemar.jpg')",
+    player: "url('./assets/img/lemar.jpg')",
+    singer: 'Lemar',
+    album: 'Time To Grow',
+    composition: "If There's Any Justice"
+  }  
 ];
 
 let i = 0;
@@ -32,9 +86,46 @@ let trackDuration = document.querySelector('.track-duration');
 let trackCurrentTime = document.querySelector('.current-time');
 const nextBtn = document.querySelector('.next');
 const prevBtn = document.querySelector('.prev');
+const containerBackground = document.querySelector('.container-bg');
+const playerBackground = document.querySelector('.player__bg');
+const album = document.querySelector('.album');
+const singer = document.querySelector('.singer');
+const composition = document.querySelector('.composition');
 
 
 // functions
+function playTrack(obj) { 
+  showBackground(obj);
+  showDataTrack(obj);
+}
+function showBackground(obj) { 
+  containerBackground.style.backgroundImage = `${obj.container}`;
+  playerBackground.style.backgroundImage = `${obj.player}`;
+}
+function showDataTrack(obj) { 
+  album.innerHTML = obj.album;
+  showShortSinger(obj);
+  showShortComposition(obj);
+}
+function showShortSinger(obj) { 
+  let str = '';
+  if (obj.singer.length > 20) {
+    str = `${obj.singer.slice(0, 20).concat() + '...'}`;
+    singer.innerHTML = str;
+  } else {
+    singer.innerHTML = obj.singer;
+  }
+}
+function showShortComposition(obj) { 
+  let str = '';
+  if (obj.composition.length > 15) {
+    str = `${obj.composition.slice(0, 14).concat() + '...'}`;
+    composition.innerHTML = str;
+  } else {
+    composition.innerHTML = obj.composition;
+  }
+}
+
 function togglePlayPause() { 
   if (audio.paused) {
     btnPlayPause.className = 'play-pause pause';
@@ -44,7 +135,10 @@ function togglePlayPause() {
     audio.pause();
   }
 }
-
+function showTimeRange() {
+  timeRange.value = audio.currentTime / audio.duration;
+}
+  
 function scrub(e) { 
   const scrubTime = (e.offsetX / timeRange.offsetWidth) * audio.duration;
   audio.currentTime = scrubTime;
@@ -60,10 +154,6 @@ function showDurationTrack(obj) {
   }
  
   trackDuration.innerHTML = time;
-
-
-  // trackDuration.innerHTML = `${Math.floor(audio.duration / 60) + ":" + Math.round(audio.duration % 60)}`;
-  
 }
 
 function showCurrentTimeTrack() { 
@@ -75,9 +165,6 @@ function showCurrentTimeTrack() {
   }
 
   trackCurrentTime.innerHTML = time;
-  
-  
-//   trackCurrentTime.innerHTML = `${Math.floor(audio.currentTime / 60) + ":" + Math.round(audio.currentTime % 60)}`;
 }
 
 function prevTrack() {  
@@ -89,16 +176,25 @@ function prevTrack() {
   track = arrSongs[i];
   audio.src = track.audioFile;
   audio.play();
+  playTrack(track);
+}
+function nextTrack() { 
+  if (i === arrSongs.length - 1) {
+    i = 0;
+  } else { 
+    i++;
+  }
+  track = arrSongs[i];
+  audio.src = track.audioFile;
+  audio.play();
+  playTrack(track);
 }
 
 // events
-
-
-
-
+playTrack(track);
 btnPlayPause.addEventListener('click', togglePlayPause);
-audio.addEventListener('timeupdate', function () {
-  timeRange.value = audio.currentTime / audio.duration; //положение ползунка
+audio.addEventListener('timeupdate', function () { // вынести в updateData
+  showTimeRange();
   showCurrentTimeTrack();
   showDurationTrack();
   
@@ -106,8 +202,8 @@ audio.addEventListener('timeupdate', function () {
 
 timeRange.addEventListener('click', scrub);
 prevBtn.addEventListener('click', prevTrack);
+nextBtn.addEventListener('click', nextTrack);
 
-console.log(trackDuration.innerHTML);
-console.log(audio.onloadedmetadata);
+
 
 
